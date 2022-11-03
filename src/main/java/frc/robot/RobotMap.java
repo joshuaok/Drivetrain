07448c0204ee1aa1540a883ce57b38a8bf4f57bf
joshuaok <joshua.o@prhsrobotics.com>
2022-11-03 19:17:20 -0400

    bugs

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,12 +1,13 @@
 package frc.robot;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class RobotMap {
-    public static final int LEFT_FRONT_DRIVE_PORT = 3;
-    public static final int RIGHT_FRONT_DRIVE_PORT = 4;
-    public static final int LEFT_BACK_DRIVE_PORT = 1;
-    public static final int RIGHT_BACK_DRIVE_PORT = 2;
     
+    public static final int Left_front_drive_port=3;
+    public static final int Right_front_drive_port=4;
+    public static final int Left_back_drive_port=1;
+    public static final int Right_back_drive_port=2;
     public static WPI_TalonFX leftFrontDrivePort= new WPI_TalonFX(Left_front_drive_port);
     public static WPI_TalonFX rightFrontDrivePort= new WPI_TalonFX(Right_front_drive_port);
     public static WPI_TalonFX leftBackDrivePort= new WPI_TalonFX(Left_back_drive_port);
